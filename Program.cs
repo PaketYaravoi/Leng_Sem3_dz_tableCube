ba271406f01f8418f216c3257f890f91bd00637b
Roman <roma.klimov10.94@gmail.com>
2023-05-03 13:56:20 +1100

    создал локалку, создал программу , пойду создам удаленный реп

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+3 → 1,4,9
+
+*/
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= number; i++)
+{
+    int newNumber = (int)Math.Pow(i, 3); // (int) - строгое преобразование , че бы там не было мы приказываем ему стать int -om
+    Console.WriteLine(newNumber);
+}
